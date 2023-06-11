1ab9c90c351c87a311fa4ca4c535be8fa5c3488e
Arko <47105136+arkaprovob@users.noreply.github.com>
2023-06-11 17:16:19 +0530

    Set up security configuration using a local Keycloak instance for OAuth2 validation.
    This commit contains the changes related to setting up security configuration and specifies the use of a local Keycloak instance for incoming request validation.

---
+++ src/main/java/com/gamedoora/gateway/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.gamedoora.gateway.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+
+@EnableWebSecurity
+public class SecurityConfig{
+
+    @Bean
+    public SecurityFilterChain  filterChain(HttpSecurity http) throws Exception {
+
+
+        http
+                .authorizeRequests(authz -> authz
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults());
+
+        //http.authorizeHttpRequests(Customizer.withDefaults()).oauth2Login(Customizer.withDefaults());
+
+
+        return http.build();
+    }
+
+}
+
