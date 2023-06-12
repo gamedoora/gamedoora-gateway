ef066cc098b3490ca44aad0df1ed9b91e75c8c1c
Arko <47105136+arkaprovob@users.noreply.github.com>
2023-06-12 13:04:23 +0530

    This commit adds a sample REST resource that can be used to test the authentication functionality of the API Gateway.
    The resource provides a simple endpoint that demonstrates the authentication flow and returns a response indicating successful authentication.

---
+++ src/main/java/com/gamedoora/gateway/api/AppStatus.java
@@ -0,0 +1,24 @@
+package com.gamedoora.gateway.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:8181")
+@RestController
+@RequestMapping("/api")
+public class AppStatus {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> status() {
+        return new ResponseEntity<>("Up & Running", HttpStatus.OK);
+    }
+
+    @GetMapping("/secret")
+    public ResponseEntity<String> secret() {
+        return new ResponseEntity<>("Shhh..", HttpStatus.OK);
+    }
+}
