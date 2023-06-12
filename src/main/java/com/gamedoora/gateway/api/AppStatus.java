ef4112f4e294405640d49dbb642a97921ecf7304
Arko <47105136+arkaprovob@users.noreply.github.com>
2023-06-12 19:00:04 +0530

    Refactor: Removed CrossOrigin settings for implementation-specific behavior

--- src/main/java/com/gamedoora/gateway/api/AppStatus.java
+++ src/main/java/com/gamedoora/gateway/api/AppStatus.java
@@ -2,12 +2,11 @@ package com.gamedoora.gateway.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:8181")
+
 @RestController
 @RequestMapping("/api")
 public class AppStatus {
