d665f81b3bb20293e35d49fb0eadd21fb08ee95c
Arko <47105136+arkaprovob@users.noreply.github.com>
2023-06-12 13:00:51 +0530

    This commit implements the necessary security configuration for the API Gateway with OAuth2.
    The changes ensure secure authentication mechanisms for accessing the gateway and its protected resources.

--- src/main/java/com/gamedoora/gateway/config/SecurityConfig.java
+++ src/main/java/com/gamedoora/gateway/config/SecurityConfig.java
@@ -1,27 +1,38 @@
 package com.gamedoora.gateway.config;
 
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
-
-
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
-
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 
 
-@EnableWebSecurity
+
+@Configuration
 public class SecurityConfig{
 
     @Bean
-    public SecurityFilterChain  filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(Customizer.withDefaults())
-                .oauth2Login(Customizer.withDefaults());
-        return http.build();
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(getAuthorizeExchangeSpecCustomizer())
+                .oauth2Login(withDefaults())
+                .build();
     }
+
+    private static Customizer<ServerHttpSecurity.AuthorizeExchangeSpec> getAuthorizeExchangeSpecCustomizer() {
+        return exchange -> exchange
+                .pathMatchers(HttpMethod.GET,
+                        "/*.html","/api/health"
+                ).permitAll()
+                .anyExchange().authenticated();
+    }
+
+
 
 }
 
